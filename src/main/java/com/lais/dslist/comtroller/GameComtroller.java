c30db12c922b6d1c1b48cc6197d294ee3890ffa1
Lais <lais060788@gmail.com>
2025-01-23 20:03:41 -0300

    Get game by id

--- src/main/java/com/lais/dslist/comtroller/GameComtroller.java
+++ src/main/java/com/lais/dslist/comtroller/GameComtroller.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lais.dslist.dto.GameDTO;
 import com.lais.dslist.dto.GameMinDTO;
 import com.lais.dslist.services.GameService;
 
@@ -18,6 +20,12 @@ public class GameComtroller {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		GameDTO result = gameService.findById(id);
+		return result;
+		}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
--- src/main/java/com/lais/dslist/services/GameService.java
+++ src/main/java/com/lais/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lais.dslist.dto.GameDTO;
 import com.lais.dslist.dto.GameMinDTO;
 import com.lais.dslist.entities.Game;
 import com.lais.dslist.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return  new  GameDTO(result);
+		
+		}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 	       List<Game> result = gameRepository.findAll();
 	       List<GameMinDTO> dto = result.stream().map (x -> new GameMinDTO(x)).toList();
